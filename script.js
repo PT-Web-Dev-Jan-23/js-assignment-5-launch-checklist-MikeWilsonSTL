d6954fd917197480225c72cfffbaed79c0c9c7ef
Mike Wilson <mikewilsonstl@gmail.com>
2023-03-09 18:18:07 -0600

    Finished everything but the JSON component so far

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 // Write your JavaScript code here!
 
-window.addEventListener("load", function() {
+// const { pickPlanet, addDestinationInfo } = require("./scriptHelper");
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
-   
+window.addEventListener("load", function () {
+    //Dear god just to relieve the eye strain
+    document.body.style.backgroundColor = "#181a1b";
+    document.body.style.color = "#e2e6e3";
+    document.getElementById("launchForm").style.backgroundColor = "#1e2729";
+    document.getElementById("launchStatusCheck").style.backgroundColor = "#1e2729";
+    document.getElementById("missionTarget").style.backgroundColor = "#1e2729";
+    document.getElementById("launchForm").style.borderColor = "#8c8273";
+    document.getElementById("launchStatusCheck").style.borderColor = "#8c8273";
+    document.getElementById("missionTarget").style.borderColor = "#8c8273";
+
+    let listedPlanets; // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+    let listedPlanetsResponse = myFetch();
+    console.log(`listedPlantesResponse = ${listedPlanetsResponse}`);
+
+    listedPlanetsResponse.then(function (result) {
+        listedPlanets = result;
+        console.log(`listedPlanets 1 = ${listedPlanets}`);
+    }).then(function () {
+        console.log(`listedPlanets 2 = ${listedPlanets}`);
+        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+        let chosenPlanet = pickPlanet(listedPlanets);
+        addDestinationInfo(document, chosenPlanet.name, chosenPlanet.diameter, chosenPlanet.star, chosenPlanet.distance, chosenPlanet.moons, chosenPlanet.image);
+    })
+    document.getElementById("formSubmit").addEventListener("click", function () {
+        formSubmission(document, pilotName.value, copilotName.value, fuelLevel.value, cargoMass.value); //Had test as second parameter and I have no idea why
+    });
 });
